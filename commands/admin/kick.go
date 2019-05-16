1eb37a8049621b26e32c635c4aeac5b3e5eadee6
Credomane <bb00200@gmail.com>
2019-05-16 15:41:58 -0500

    Fix incorrect comments for kick/ban/unban functions

--- commands/admin/kick.go
+++ commands/admin/kick.go
@@ -6,7 +6,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// SaveServer executes the save command on the server.
+// KickPlayer executes the kick command on the server.
 func KickPlayer(s *discordgo.Session, m *discordgo.MessageCreate, arg1 string, arg2 string) {
 	io.WriteString(*P, "/kick "+arg1+" "+arg2+"\n")
 	s.ChannelMessageSend(m.ChannelID, "Player "+arg1+" kicked with reason "+arg2+"!")
--- commands/admin/unban.go
+++ commands/admin/unban.go
@@ -6,7 +6,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// SaveServer executes the save command on the server.
+// UnbanPlayer executes the unban command on the server.
 func UnbanPlayer(s *discordgo.Session, m *discordgo.MessageCreate, arg1 string) {
 	io.WriteString(*P, "/unban "+arg1+"\n")
 	s.ChannelMessageSend(m.ChannelID, "Player "+arg1+" unbanned!")
